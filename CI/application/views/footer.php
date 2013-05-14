b6620e291456a0052cadf73e21f5e713a2c1fc87
Jason Cotter <jasoncotter@fullsail.edu>
2013-05-14 19:39:05 -0400

    2.1 last minute changes
    Corrected html in views.

--- CI/application/views/footer.php
+++ CI/application/views/footer.php
@@ -1,24 +1,7 @@
-	<footer id="footer">
-	
+	<footer>	
 
-	
-	<h3 class="socialtag">Desperate for more Jared content?</h3>
-	
-	
-	<ul>
-	
-		<li class="social"><a href="http://www.facebook.com/jaredcotter"><img src="images/facebook.png" alt="Facebook Link"> </a></li>
 		
-		<li class="social"><a href="http://twitter.com/#!/JAREDCOTTER"><img src="images/imdb.png" alt="Twitter Link"> </a></li>
-		
-		<li class="social"><a href="http://www.imdb.com/name/nm2554073/"><img src="images/twitter.png" alt="IMDB Link"> </a></li>
-		
-	</ul>
-	
-		
-	<p class="copyright">All Rights Reserved. &copy;2012</p>
-	
 	</footer>
-	</div> <!--wrapper-->
+
 </body>
 </html>
---
+++ CI/application/views/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>/JapanPics</title>
+	<style type="text/css">
+	@import url(css/main.css) screen;
+	</style>
+</head>
